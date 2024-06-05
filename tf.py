cf7601c409001a147e96f94d41c62211c107ae7f
robotiger <palmov-a@ya.ru>
2024-06-05 19:48:02 +0400

    now

--- tf.py
+++ tf.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for,  send_file, Response
+from flask import Flask, render_template, request, redirect, url_for,  send_file, Response, jsonify
 from flask_bootstrap import Bootstrap
+from werkzeug.middleware.proxy_fix import ProxyFix
 import threading
 import time
 import sqlitedict
 import openpyxl as xl
+import ifcfg
+
 
 app = Flask(__name__)
 app.secret_key = 'your_ssecret_key'
@@ -12,6 +15,8 @@ Bootstrap(app)
 
 config=sqlitedict.SqliteDict('config_tf.db')
 config.autocommit=True
+
+wanipname ="/home/bfg/data/wanip.conf"      
 
 for x in config:
     print(x, config[x])
@@ -36,13 +41,9 @@ tab={ "snum":{"name":"Номер теста","typ":int,"cla":""},
 "shrink":{"name":"Усадка","typ":float,"cla":"table-primary"},
     }
 
-def tph(t):
-    if t==float:
-        return "number step=0.001"
-    if t==int:
-        return "number"
-    if t==str:
-        return "text"
+
+tp_status={"progress":0,"cycles_done":0,"to_do":"nothing"}
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -63,52 +64,65 @@ def index():
     data={}
     for x in tab:
         data[x]=config.get(x,'')
-        
-    data['docycles']=150000
-    if data['cycles']>0:
-        config['progress']=data['docycles']*100//data['cycles']
-    else:
-        config['progress']=0
-    return render_template('indexf.html',**data)
+    return render_template('index.html',**data|tp_status)
 
-@app.route('/newtest', methods=['GET', 'POST'])
+@app.route('/status')
+def sendstatus():
+    data={}
+    for x in tab:
+        data[x]=config.get(x,'')   
+    return jsonify(data|tp_status)
+
+@app.route('/newtest', methods=['GET'])
 def execute_newtest():
     config['snum']=config.get('snum',1)+1
     config['xlname']=f'sp{config["snum"]:06d}.xlsx'
-    wb=xl.load_workbook('sp.xlsx')
+    #wb=xl.load_workbook('sp.xlsx')
+    wb=xl.Workbook()
     wb.save(config['xlname'])
+    tp_todo="newtest"
     return redirect(url_for('index'))
 
-@app.route('/setspring', methods=['GET', 'POST'])
+@app.route('/setspring', methods=['GET'])
 def execute_setspring():
-    print('start setspring')
-    ra='setspring'
+    tp_todo="setspring"
     return redirect(url_for('index'))
 
-@app.route('/runtest', methods =['GET', 'POST'])
+@app.route('/runtest', methods =['GET'])
 def execute_runtest():
-    print('start setspring')
-    ra='setspring'
+    tp_todo="runtest"
     return redirect(url_for('index'))
 
-@app.route('/stoptest')
+@app.route('/stoptest',methods =['GET'])
 def execute_stoptest():
-    print('start setspring')
-    ra='setspring'
+    tp_todo="stoptest"
     return redirect(url_for('index'))
 
-@app.route('/download')
+@app.route('/download',methods =['GET'])
 def execute_download():
     return send_file(config['xlname'], as_attachment=True)
 
 
-@app.route('/progress')
+@app.route('/progress',methods =['GET'])
 def progress():
     def generate():
         while True:
-            yield f"data:{config['progress']}\n\n"
-            time.sleep(5)
+            yield f"data:{g.counter}\n\n"
+            
+            #yield f"data:{tp_status['progress']}\n\n"
+            time.sleep(1)
     return Response(generate(), mimetype= 'text/event-stream')
+
+@app.route('/update_software',methods =['GET'])
+def execute_update():
+    tp_todo="update_software"
+    return redirect(url_for('index'))
+
+@app.route('/reboot',methods =['GET'])
+def execute_reboot():
+    '            <li class="nav-item"> <a class="nav-link"          href="/reboot">Перезагрузить</a>  </li>'
+    tp_todo="reboot"
+    return redirect(url_for('index'))
 
 
 class gp(threading.Thread):
@@ -125,12 +139,17 @@ class gp(threading.Thread):
     def stop(self):
         self.stp=True
         
-        
 
 
 if __name__ == '__main__':
     g=gp()
     g.start()
+    
+    #обновить ip адрес для сервера nginx
+    with open(wanipname, "w") as the_file: 
+        the_file.write(f"server_name {' '.join([x[1]['inet'] for x in ifcfg.interfaces().items()])};\n")
+        
+    app.wsgi_app = ProxyFix(app.wsgi_app)    
     app.run(debug=True)
     
 
